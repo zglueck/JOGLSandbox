80cc538ea2cfcdfe277de9dea667ac573955a031
Zach Glueckert <zach.glueckert@missiondrivenresearch.com>
2016-09-26 22:52:07 -0400

    Comparison demo

--- src/main/java/zone/glueck/glstuff/GLSimpleStuff.java
+++ src/main/java/zone/glueck/glstuff/GLSimpleStuff.java
@@ -14,12 +14,12 @@ import gov.nasa.worldwind.geom.Angle;
 import gov.nasa.worldwind.geom.Matrix;
 import gov.nasa.worldwind.geom.Vec4;
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.io.IOException;
 import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
 import java.nio.charset.StandardCharsets;
-import javax.media.opengl.GL2;
 import javax.media.opengl.GL3;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCapabilities;
@@ -31,13 +31,11 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 /**
- *
- * @author Zach Glueckert <zach.glueckert@missiondrivenresearch.com>
  */
-public class GLSimpleStuff implements GLEventListener {
+public class GLSimpleStuff implements GLEventListener, KeyListener {
     
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 700;
     public static final int FPS = 60;
 
     private final GLCanvas canvas;
@@ -52,14 +50,14 @@ public class GLSimpleStuff implements GLEventListener {
         this.canvas.addGLEventListener(this);
         this.animator = new FPSAnimator(this.canvas, FPS);
         
-        final JFrame jframe = new JFrame( "Simple GL Proving Grounds" ); 
+        final JFrame jframe = new JFrame( "Simple GL Proving Grounds" );
         jframe.addWindowListener( new WindowAdapter() {
             public void windowClosing( WindowEvent windowevent ) {
                 animator.stop();
                 jframe.dispose();
             }
         });
-
+        canvas.addKeyListener(this);
         jframe.getContentPane().add( this.canvas, BorderLayout.CENTER );
         jframe.setSize( WIDTH, HEIGHT );
         jframe.setVisible( true );
@@ -69,62 +67,137 @@ public class GLSimpleStuff implements GLEventListener {
         
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // nothing
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_UP:
+                this.pitched = this.pitched - this.step;
+                break;
+            case KeyEvent.VK_DOWN:
+                this.pitched = this.pitched + this.step;
+                break;
+            case KeyEvent.VK_LEFT:
+                this.yaw = this.yaw - this.step;
+                break;
+            case KeyEvent.VK_RIGHT:
+                this.yaw = this.yaw + this.step;
+                break;
+            case KeyEvent.VK_A:
+                this.zoom = this.zoom - this.step;
+                break;
+            case KeyEvent.VK_Z:
+                this.zoom = this.zoom + this.step;
+                break;
+        }
+
+    }
+    protected double step = 5.0;
+    protected double pitched = 0.0;
+    protected double yaw = 0.0;
+    protected double zoom = 60.0;
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // nothing
+    }
+
     //private int vao;
     private int vboBox;
     private int vboFloor;
+    private int vboCpu;
     private int ebo;
     private int program;
-    private int stipScaleLocation;
+    private int cpuProgram;
+    private int eyeScalingFactorLocation;
     private int textureUniformLocation;
     private int mvpLocation;
+    private int cpuMvpLocation;
+    private int cpuLinTextCoordLocation;
     private Texture textureImage;
+    private double eyeDistance = 60.0;
     
     private final float[] box = {
-        -10f, -10f, 0f, 1f, 0f, 0f, 1f, 0f,
-        10f, -10f, 0f, 0f, 1f, 0f, 1f, 0f,
-        10f,  10f,  0f, 0f, 1f, 0f, 0f, 0f,
-        -10f,  10f,  0f, 0.1f, 0.2f, 0.8f, 0f, 0f
+        -10f,  10f,  0f, 1f, 1f, 1f, 0f, 0f,
+        -10f, -10f, 0f, 1f, 1f, 1f, 20f, 0f,
+        10f,  10f,  0f, 1f, 1f, 1f,(float) (Math.sqrt(2)*20.0+20.0), 0f,
+        10f, -10f, 0f, 1f, 1f, 1f, (float) (Math.sqrt(2)*20.0+40.0), 0f
     };
     
     private final float[] floor = {
-        -10.5f, -10.5f, 0f, 1f, 1f, 1f, 0f, 0f,
-        10.5f, -10.5f, 0f, 1f, 1f, 1f, 0f, 0f,
-        10.5f, 10.5f, 0f, 1f, 1f, 1f, 0f, 0f,
-        -10.5f, 10.5f, 0f, 1f, 1f, 1f, 0f, 0f,
+        -10.5f,  10f,  -1f, 1f, 0f, 0f, 0f, 0f,
+        -10.5f, -10f, -1f, 1f, 0f, 0f, 20f, 0f,
+        9.5f,  10f,  -1f, 1f, 0f, 0f, (float) (Math.sqrt(2)*20.0+20.0), 0f,
+        9.5f, -10f, -1f, 1f, 0f, 0f, (float) (Math.sqrt(2)*20.0+40.0), 0f
     };
     
-    private final int[] idx = {
-        3,0,2,1
+    private final Vec4[] cpuPos = new Vec4[]{
+        new Vec4(-9.5, 10.0, 1.0),
+        new Vec4(-9.5, -10.0, 1.0),
+        new Vec4(10.5, 10.0, 1.0),
+        new Vec4(10.5, -10.0, 1.0)
     };
-
-    private final String vertShaderSource = 
+    
+//    private final int[] idx = {
+//        0, 1, 2, 3
+//    };
+    
+    private static final String cpuVertShaderSource = 
         "#version 150 core\n" +
         "in vec3 position;\n" +
         "in vec3 color;\n" +
-        "in vec2 texcoord;\n" +
+        "in float lintextcoord;\n" +
         "uniform mat4 mvp;\n" +
         "out vec3 Color;\n" +
         "out vec2 Texcoord;\n" +
         "void main()\n" +
         "{\n" +
         "    Color = color;\n" +
-        "    Texcoord = texcoord;\n" +
+        "    Texcoord = vec2(lintextcoord, 0.0);\n" +
         "    gl_Position = mvp * vec4(position, 1.0);\n" +
         "}";
-    
-    private final String fragShaderSource = 
+
+    private static final String cpuFragShaderSource = 
         "#version 150 core\n" +
         "in vec3 Color;\n" +
         "in vec2 Texcoord;\n" +
         "out vec4 outColor;\n" +
-        "uniform float stipScale;\n" +
         "uniform sampler2D tex;\n" +
         "void main()\n" +
         "{\n" +
-        //"    vec4 alpha = texture(tex, TexCoord).aaaa;\n" +
-        //"    outColor = alpha*vec4(Color, 1.0);\n" +
-        "    outColor = vec4(Color, 1.0) * texture(tex, Texcoord * stipScale);\n" +
-        //"    outColor = texture(tex, Texcoord);\n" +
+        "    outColor = vec4(Color, 1.0) * texture(tex, Texcoord);\n" +
+        "}";
+    
+    private static final String vertShaderSource = 
+        "#version 150 core\n" +
+        "in vec3 position;\n" +
+        "in vec3 color;\n" +
+        "in vec2 texcoord;\n" +
+        "uniform mat4 mvp;\n" +
+        "uniform float eyeScalingFactor;\n" +
+        "out vec3 Color;\n" +
+        "out vec2 Texcoord;\n" +
+        "void main()\n" +
+        "{\n" +
+        "    Color = color;\n" +
+        "    Texcoord = vec2(texcoord.x/eyeScalingFactor, 0.0);\n" +
+        "    gl_Position = mvp * vec4(position, 1.0);\n" +
+        "}";
+    
+    private static final String fragShaderSource = 
+        "#version 150 core\n" +
+        "in vec3 Color;\n" +
+        "in vec2 Texcoord;\n" +
+        "out vec4 outColor;\n" +
+        "uniform sampler2D tex;\n" +
+        "void main()\n" +
+        "{\n" +
+        "    outColor = vec4(Color, 1.0) * texture(tex, Texcoord);\n" +
         "}";
     
     @Override
@@ -134,7 +207,7 @@ public class GLSimpleStuff implements GLEventListener {
         
         int[] a = new int[3];
         
-        gl.glGenBuffers(3, a, 0);
+        gl.glGenBuffers(a.length, a, 0);
         this.vboFloor = a[0];
         gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, this.vboFloor);
         FloatBuffer stippleBuffer = Buffers.newDirectFloatBuffer(floor);
@@ -147,19 +220,23 @@ public class GLSimpleStuff implements GLEventListener {
         boxBuffer.flip();
         gl.glBufferData(GL3.GL_ARRAY_BUFFER, box.length * Buffers.SIZEOF_FLOAT, boxBuffer, GL3.GL_STATIC_DRAW);
         
-        this.ebo = a[2];
-        gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
-        IntBuffer idxBuffer = Buffers.newDirectIntBuffer(idx);
-        idxBuffer.flip();
-        gl.glBufferData(GL3.GL_ELEMENT_ARRAY_BUFFER, idx.length * Buffers.SIZEOF_INT, idxBuffer, GL3.GL_STATIC_DRAW);
+//        this.ebo = a[2];
+//        gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
+//        IntBuffer idxBuffer = Buffers.newDirectIntBuffer(idx);
+//        idxBuffer.flip();
+//        gl.glBufferData(GL3.GL_ELEMENT_ARRAY_BUFFER, idx.length * Buffers.SIZEOF_INT, idxBuffer, GL3.GL_STATIC_DRAW);
+        
+        this.vboCpu = a[2];
+        gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, this.vboCpu);
+        gl.glBufferData(GL3.GL_ARRAY_BUFFER, 28 * Buffers.SIZEOF_FLOAT, null, GL3.GL_STREAM_DRAW);
         
         int v = gl.glCreateShader(GL3.GL_VERTEX_SHADER);
-        gl.glShaderSource(v, 1, getVertShader(), null, 0);
+        gl.glShaderSource(v, 1, Shader.VERT.getShader(), null, 0);
         gl.glCompileShader(v);
         this.checkCompileShaderStatus(gl, v);
         
         int f = gl.glCreateShader(GL3.GL_FRAGMENT_SHADER);
-        gl.glShaderSource(f, 1, getFragShader(), null, 0);
+        gl.glShaderSource(f, 1, Shader.FRAG.getShader(), null, 0);
         gl.glCompileShader(f);
         this.checkCompileShaderStatus(gl, f);
         
@@ -173,15 +250,29 @@ public class GLSimpleStuff implements GLEventListener {
         gl.glValidateProgram(this.program);
         this.checkValidation(gl);
         
-        this.stipScaleLocation = gl.glGetUniformLocation(this.program, "stipScale");
+        this.eyeScalingFactorLocation = gl.glGetUniformLocation(this.program, "eyeScalingFactor");
         this.mvpLocation = gl.glGetUniformLocation(this.program, "mvp");
         
-//        gl.glGenVertexArrays(1, a, 0);
-//        this.vao = a[0];
-//        gl.glBindVertexArray(this.vao);
+        v = gl.glCreateShader(GL3.GL_VERTEX_SHADER);
+        gl.glShaderSource(v, 1, Shader.C_VERT.getShader(), null, 0);
+        gl.glCompileShader(v);
+        this.checkCompileShaderStatus(gl, v);
         
+        f = gl.glCreateShader(GL3.GL_FRAGMENT_SHADER);
+        gl.glShaderSource(f, 1, Shader.C_FRAG.getShader(), null, 0);
+        gl.glCompileShader(f);
+        this.checkCompileShaderStatus(gl, f);
         
-        
+        this.cpuProgram = gl.glCreateProgram();
+        gl.glAttachShader(this.cpuProgram, v);
+        gl.glAttachShader(this.cpuProgram, f);
+        gl.glBindFragDataLocation(this.cpuProgram, 0, "outColor");
+        gl.glLinkProgram(this.cpuProgram);
+        this.checkLinkStatus(gl);
+        gl.glUseProgram(this.cpuProgram);
+        gl.glValidateProgram(this.cpuProgram);
+        this.checkValidation(gl);
+  
         try {
             this.textureImage = TextureIO.newTexture(this.getClass().getResourceAsStream("/4x4_White_Checkerboard.png"), false, ".png");
             if (this.textureImage == null) {
@@ -220,18 +311,18 @@ public class GLSimpleStuff implements GLEventListener {
     }
     double cameraRotationRadians = 0.0;
     double modelRotationRadians = 0.0;
+    double iniDist = -Double.MAX_VALUE;
     @Override
     public void display(GLAutoDrawable glad) {
         
         // Matrix Setup
-        Vec4 eyePoint = new Vec4(Math.sin(cameraRotationRadians)*(60.0), 5.0, Math.cos(cameraRotationRadians)*(60.0));
-        Matrix modelBox = Matrix.fromRotationX(Angle.fromRadians(this.modelRotationRadians));
-        Matrix modelStipple = Matrix.IDENTITY;
-        //Matrix model = Matrix.IDENTITY;
+        Vec4 eyePoint = new Vec4(0.0, 0.0, this.zoom);
+        Matrix modelBox = Matrix.fromRotationXYZ(Angle.fromDegrees(this.pitched), Angle.fromDegrees(this.yaw), Angle.ZERO);
+        // Matrix modelBox = Matrix.fromRotationX(Angle.fromRadians(this.modelRotationRadians));
         Matrix modelView = Matrix.fromViewLookAt(eyePoint, Vec4.ZERO, Vec4.UNIT_Y);
-        Matrix projection = Matrix.fromPerspective(Angle.fromDegrees(45.0), WIDTH, HEIGHT, 1.0, 100.0);
+        Matrix projection = Matrix.fromPerspective(Angle.fromDegrees(45.0), WIDTH, HEIGHT, 1.0, 1000.0);
         Matrix mvpBox = projection.multiply(modelView.multiply(modelBox));
-        Matrix mvpStipple = projection.multiply(modelView.multiply(modelStipple));
+        Matrix mvpStipple = projection.multiply(modelView.multiply(modelBox));
         float[] mvpFloat = new float[16];
         double[] mvpDouble = new double[16];
         mvpBox.toArray(mvpDouble, 0, true);
@@ -252,7 +343,7 @@ public class GLSimpleStuff implements GLEventListener {
         // First Draw
         //gl.glBindVertexArray(this.vao);
         gl.glUseProgram(this.program);
-        gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
+        //gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
         gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, this.vboBox);
         
         int posAttrib = gl.glGetAttribLocation(this.program, "position");
@@ -272,46 +363,100 @@ public class GLSimpleStuff implements GLEventListener {
         this.textureImage.enable(gl);
         this.textureImage.bind(gl);
         gl.glUniform1i(this.textureUniformLocation, 0);
-        gl.glUniform1f(this.stipScaleLocation, 10f);
+        if (this.iniDist==-Double.MAX_VALUE){
+            this.iniDist = eyePoint.getLength3()/60.0;
+        }
+        gl.glUniform1f(this.eyeScalingFactorLocation, (float) (eyePoint.getLength3()/60.0));
         gl.glUniformMatrix4fv(this.mvpLocation, 1, true, mvpFloat, 0);
         
-        gl.glDrawElements(GL3.GL_LINE_STRIP, 4, GL3.GL_UNSIGNED_INT, 0);
+        gl.glDrawArrays(GL3.GL_LINE_STRIP, 0, 4);
+        //gl.glDrawElements(GL3.GL_LINE_STRIP, 4, GL3.GL_UNSIGNED_INT, 0);
         
         this.textureImage.disable(gl);
         
         // Second Draw
-//        //gl.glBindVertexArray(this.vao);
-//        gl.glUseProgram(this.program);
-//        gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
-//        gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, this.vboFloor);
-//        
-//        gl.glEnableVertexAttribArray(posAttrib);
-//        gl.glVertexAttribPointer(posAttrib, 3, GL3.GL_FLOAT, false, 8 * Buffers.SIZEOF_FLOAT, 0);
-//        
-//        gl.glEnableVertexAttribArray(texAttrib);
-//        gl.glVertexAttribPointer(texAttrib, 2, GL3.GL_FLOAT, false, 8 * Buffers.SIZEOF_FLOAT, 6 * Buffers.SIZEOF_FLOAT);
-//        
-//        gl.glEnableVertexAttribArray(colAttrib);
-//        gl.glVertexAttribPointer(colAttrib, 3, GL3.GL_FLOAT, false, 8 * Buffers.SIZEOF_FLOAT, 3 * Buffers.SIZEOF_FLOAT);
-//        
-//        gl.glActiveTexture(GL3.GL_TEXTURE0);
-//        this.textureImage.enable(gl);
-//        this.textureImage.bind(gl);
-//        gl.glUniform1i(this.textureUniformLocation, 0);
-//        gl.glUniform1f(this.stipScaleLocation, 1f);
-//        mvpStipple.toArray(mvpDouble, 0, true);
-//        for(int i = 0; i<16; i++){
-//            mvpFloat[i] = (float) mvpDouble[i];
-////        }
-//        gl.glUniformMatrix4fv(this.mvpLocation, 1, true, mvpFloat, 0);
-//        
-//        gl.glDrawElements(GL3.GL_LINE_STRIP, 4, GL3.GL_UNSIGNED_INT, 0);
-//        
-//        this.textureImage.disable(gl);
-
-        // Modify world
-        this.cameraRotationRadians += 0.01;
-        this.modelRotationRadians +=0.003;
+        //gl.glBindVertexArray(this.vao);
+        gl.glUseProgram(this.program);
+        //gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
+        gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, this.vboFloor);
+        
+        gl.glEnableVertexAttribArray(posAttrib);
+        gl.glVertexAttribPointer(posAttrib, 3, GL3.GL_FLOAT, false, 8 * Buffers.SIZEOF_FLOAT, 0);
+        
+        gl.glEnableVertexAttribArray(texAttrib);
+        gl.glVertexAttribPointer(texAttrib, 2, GL3.GL_FLOAT, false, 8 * Buffers.SIZEOF_FLOAT, 6 * Buffers.SIZEOF_FLOAT);
+        
+        gl.glEnableVertexAttribArray(colAttrib);
+        gl.glVertexAttribPointer(colAttrib, 3, GL3.GL_FLOAT, false, 8 * Buffers.SIZEOF_FLOAT, 3 * Buffers.SIZEOF_FLOAT);
+        
+        gl.glActiveTexture(GL3.GL_TEXTURE0);
+        this.textureImage.enable(gl);
+        this.textureImage.bind(gl);
+        gl.glUniform1i(this.textureUniformLocation, 0);
+        gl.glUniform1f(this.eyeScalingFactorLocation, (float) this.iniDist);
+        mvpStipple.toArray(mvpDouble, 0, true);
+        for(int i = 0; i<16; i++){
+            mvpFloat[i] = (float) mvpDouble[i];
+        }
+        gl.glUniformMatrix4fv(this.mvpLocation, 1, true, mvpFloat, 0);
+        
+        gl.glDrawArrays(GL3.GL_LINE_STRIP, 0, 4);
+        // gl.glDrawElements(GL3.GL_LINE_STRIP, 4, GL3.GL_UNSIGNED_INT, 0);
+        
+        this.textureImage.disable(gl);
+        
+        // Third Draw
+        //gl.glBindVertexArray(this.vao);
+        gl.glUseProgram(this.cpuProgram);
+        //gl.glBindBuffer(GL3.GL_ELEMENT_ARRAY_BUFFER, this.ebo);
+        // rebuffer the data
+        
+        float[] data = new float[28];
+        for (int i = 0; i<4; i++) {
+            data[(i * 7)] = (float) this.cpuPos[i].x;
+            data[(i * 7) + 1] = (float) this.cpuPos[i].y;
+            data[(i * 7) + 2] = (float) this.cpuPos[i].z;
+            data[(i * 7) + 3] = 0f;
+            data[(i * 7) + 4] = 1f;
+            data[(i * 7) + 5] = 0f;
+            if (i == 0) {
+                data[(i * 7) + 6] = 0f;    
+            } else {
+                Vec4 diffVector = this.cpuPos[i].subtract3(this.cpuPos[i - 1]);
+                diffVector = diffVector.transformBy4(mvpBox);
+                double scaleValue = Math.sqrt(diffVector.x * diffVector.x + diffVector.y * diffVector.y);
+                scaleValue = scaleValue * (60.0 / eyePoint.getLength3());
+                data[(i * 7) + 6] = (float) (scaleValue * 0.25 + data[((i - 1) * 7) + 6]);
+            }
+        }
+        FloatBuffer cpuBuffer = Buffers.newDirectFloatBuffer(data);
+        cpuBuffer.flip();
+        gl.glBindBuffer(GL3.GL_ARRAY_BUFFER, this.vboCpu);
+        gl.glBufferData(GL3.GL_ARRAY_BUFFER, data.length * Buffers.SIZEOF_FLOAT, cpuBuffer, GL3.GL_STREAM_DRAW);
+        
+        posAttrib = gl.glGetAttribLocation(this.cpuProgram, "position");
+        gl.glEnableVertexAttribArray(posAttrib);
+        gl.glVertexAttribPointer(posAttrib, 3, GL3.GL_FLOAT, false, 7 * Buffers.SIZEOF_FLOAT, 0);
+        
+        colAttrib = gl.glGetAttribLocation(this.cpuProgram, "color");
+        gl.glEnableVertexAttribArray(colAttrib);
+        gl.glVertexAttribPointer(colAttrib, 3, GL3.GL_FLOAT, false, 7 * Buffers.SIZEOF_FLOAT, 3 * Buffers.SIZEOF_FLOAT);
+        
+        int linTextCoordLocation = gl.glGetAttribLocation(this.cpuProgram, "lintextcoord");
+        gl.glEnableVertexAttribArray(linTextCoordLocation);
+        gl.glVertexAttribPointer(linTextCoordLocation, 1, GL3.GL_FLOAT, false, 7 * Buffers.SIZEOF_FLOAT, 6 * Buffers.SIZEOF_FLOAT);
+        
+        gl.glActiveTexture(GL3.GL_TEXTURE0);
+        this.textureImage.enable(gl);
+        this.textureImage.bind(gl);
+        gl.glUniform1i(this.textureUniformLocation, 0);
+        //gl.glUniform1f(this.eyeScalingFactorLocation, (float) this.iniDist);
+        gl.glUniformMatrix4fv(this.mvpLocation, 1, true, mvpFloat, 0);
+        
+        gl.glDrawArrays(GL3.GL_LINE_STRIP, 0, 4);
+        // gl.glDrawElements(GL3.GL_LINE_STRIP, 4, GL3.GL_UNSIGNED_INT, 0);
+        
+        this.textureImage.disable(gl);
     }
 
     @Override
@@ -399,6 +544,25 @@ public class GLSimpleStuff implements GLEventListener {
             return new String[]{this.fragShaderSource};
     }
 
+
+
+    enum Shader {
+        VERT, FRAG, C_VERT, C_FRAG;
+        
+        public String[] getShader(){
+            switch(this){
+                case VERT:
+                    return new String[]{vertShaderSource};
+                case FRAG:
+                    return new String[]{fragShaderSource};
+                case C_VERT:
+                    return new String[]{cpuVertShaderSource};
+                case C_FRAG:
+                    return new String[]{cpuFragShaderSource};
+            }
+            return null;
+        }
+    }
     
     public static void main(String[] args) {
         
